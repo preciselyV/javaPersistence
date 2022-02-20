2626185c4f460d63209dccd1c5f95faadc62ee12
TheOriginalTiger <vvp.vovarulit@gmail.com>
2022-02-20 21:00:49 +0700

    started implementing deserialization. Kinda stopped when it came clear that it can be impossible to determine which constructor to call and in what positions should arguments be

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,62 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+class tmp {
+    public String s1;
+    public String s2;
+    public String s3;
+
+    public tmp (String s1, String s2,String s3)
+    {
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+    }
+}
+
 
 public class Main {
     public static void main(String[] args){
-        Programmer ex = new Programmer("asshole","Rofl Olegovich", 23, "original");
-        Reflection.serialize(ex);
+        //Programmer ex = new Programmer("asshole","Rofl Olegovich", 23, "original");
+        /*
+        Person ex = new Person("pomogiti", 228);
+        String res = Reflection.serialize(ex);
+        System.out.println(res);
+        try
+        {
+            Reflection.deserialize(res);
+        }
+        catch (ClassNotFoundException e )
+        {
+            System.out.println("class was not found");
+        }
+        catch (NoSuchMethodException e )
+        {
+            System.out.println("apparently there is no constructor for the class");
+        }
+        */
+
+        Class<?>[] types = {String.class, String. class,String.class};
+        try {
+            Constructor<?> constructor = Class.forName("tmp").getConstructor(types);
+
+            Object[] ar = {"3","2","1"};
+            Object res = constructor.newInstance(ar);
+
+            tmp finallyRes = (tmp) res;
+            System.out.println(finallyRes.s1);
+        }
+        catch (NoSuchMethodException e)
+        {
+            System.out.println("no such method");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/Reflection.java
+++ src/main/java/Reflection.java
@@ -1,4 +1,5 @@
 import java.io.StringReader;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import javax.json.Json;
@@ -13,11 +14,13 @@ import java.util.List;
 //TODO настройка приватных полей.
 //TODO многопоточность для записи ?
 //TODO ага, jsonы тоже нужно добавить
-//TODO как подтягивать сам класс из json? Нужно понять, как найти конструктор
 //TODO если атрибут класса это тоже класс, но при этом поле помечено, а класс нет - что делать? (настроение - падать)
 //TODO а что с суперклассами то делать?
 //TODO что делать в случаях, когда указаны не все поля, которые нужны конструкору?
-//TODO придумать что то для всяких конструкторов. Наверное мы хотим хранить не целиком эти классы, а просто содержимое
+//TODO придумать что то для всяких контейнеров. Наверное мы хотим хранить не целиком эти классы, а просто содержимое
+//TODO а что делать, когда у нас не один конструктор?
+
+
 public class Reflection {
 
     private static ArrayList<Field> getFields(Class<?> cls, Object obj)
@@ -60,17 +63,7 @@ public class Reflection {
         }
     }
 
-    private static void convertToJson(ArrayList<Field> flds)
-    {
-        for (Field fld : flds)
-        {
-            String name = fld.getName();
-            System.out.println(name);
-        }
-    }
-
-
-    public static void serialize(Object obj)
+    public static String serialize(Object obj)
     {
         Class<?> cls = obj.getClass();
         String className = cls.getName();
@@ -79,22 +72,40 @@ public class Reflection {
         json.add("ClassName", className);
         if (fields != null)
         {
-
             var flds = Json.createArrayBuilder();
             for (Field field : fields)
             {
                 try
                 {
+                    //TODO check whether we change visibility for every1 else
                     field.setAccessible(true);
                     flds.add( Json.createObjectBuilder().add(field.getName(), field.get(obj).toString()));
                 }
                 catch (IllegalAccessException e)
                 {
-                    System.out.println("well, it was illegal");
+                    System.out.println("well, for some reason it was illegal");
                 }
             }
             json.add("fields", flds);
         }
-        System.out.println(json.build().toString());
+        return json.build().toString();
     }
+
+    /*
+    вообще задача сводится к кринжу. Как выбрать конструктор?
+     */
+    public static void deserialize(String jsonString) throws ClassNotFoundException, NoSuchMethodException {
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonString));
+        JsonObject object = jsonReader.readObject();
+        jsonReader.close();
+        String name = object.getString("ClassName");
+        Class<?> cls = Class.forName(name);
+        Constructor<?>[] constructors = cls.getConstructors();
+        if (constructors.length == 0 )
+        {
+            throw new NoSuchMethodException("couldn't find constructor for the class");
+        }
+
+    }
+
 }
